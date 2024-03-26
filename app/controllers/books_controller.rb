89f65543fa83412270448a15370367d0749a58ae
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-03-26 16:14:51 +0000

    [Fix]部分テンプレート定義、削除アクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,12 @@
 class BooksController < ApplicationController
 
+  def new
+    @book = Book.new
+  end 
+ 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
@@ -32,15 +37,15 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
